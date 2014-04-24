812e87e096d29c2409f1b50e5d5a879001de3f9c
nottus97 <nottus97@gmail.com>
2014-04-23 21:33:04 -0500

    User Profile updates now
    should probably use AJAX and modify for image, will come back to if
    time allows

--- users/profile.php
+++ users/profile.php
@@ -10,15 +10,25 @@ $msg = NULL;
 
 if(isset($_POST['update']))
 {
-$update = "UPDATE ".USERS." SET full_name = '".filter($_POST['fullname'])."', user_name = '".filter($_POST['username'])."', usr_email = AES_ENCRYPT('".filter($_POST['email'])."', '$salt')";
+$update = "UPDATE ".USERS." SET firstname = '".filter($_POST['firstname']).
+"', lastname = '".filter($_POST['lastname']).
+"', username = '".filter($_POST['username']).
+"', email = AES_ENCRYPT('".filter($_POST['email'])."', '$salt') ".
+", address1 = '".filter($_POST['address1']).
+"', address2 = '".filter($_POST['address2']).
+"', city = '".filter($_POST['city']).
+"', state = '".filter($_POST['state']).
+"', zipcode = '".filter($_POST['zip']).
+"', bio = '".filter($_POST['bio'])."'";
 
-if(!empty($_POST['newpass']))
-{
-$update .= ", usr_pwd = '".hash_pass(filter($_POST['newpass']))."'";
-}
 
-$update .= " WHERE id = '".$_SESSION['UserId']."'";
+// if(!empty($_POST['newpass']))
+// {
+// $update .= ", usr_pwd = '".hash_pass(filter($_POST['newpass']))."'";
+// }
 
+$update .= " WHERE id = '".$_SESSION['user_id']."'";
+// echo $update;
 $run_update = mysql_query($update) or die(mysql_error());
 
 if($run_update)
@@ -43,9 +53,10 @@ $msg = "Profile updated successfully!";
 <div class="content">
 <div class="main">
 
-<h1>Update Your Information</h1>
 
-<h2><?php echo $_SESSION['FirstName']; ?>!  Making sure your profile information is updated ensures that the most pet-owners in need get connected with the best resources. Make sure to keep your profile up-to-date at all times!</h2>
+<h2>  
+<!-- <?php echo $_SESSION['FirstName']; ?>!  -->
+Please keep your profile information updated to ensure that the most pet-owners in need get connected with the best resources. </h2>
 
 <!-- 
 <p>Form that:</p>
@@ -78,15 +89,15 @@ while($r = mysql_fetch_array($in))
 <table cellspacing="5" cellpadding="5" border="0">
 <tr>
 <td>First Name</td>
-<td><input type="text" name="FirstName" value="<?php echo $r['FirstName']; ?>" /></td>
+<td><input type="text" name="firstname" value="<?php echo $r['FirstName']; ?>" /></td>
 </tr>
    <tr>
 <td>Last Name</td>
-<td><input type="text" name="LastName" value="<?php echo $r['LastName']; ?>" /></td>
+<td><input type="text" name="lastname" value="<?php echo $r['LastName']; ?>" /></td>
 </tr>
    <tr>
 <td>Username</td>
-<td><input type="text" name="UserName" value="<?php echo $r['UserName']; ?>" /></td>
+<td><input type="text" name="username" value="<?php echo $r['UserName']; ?>" /></td>
 </tr>
 <tr>
 <td>Address 1</td>
@@ -98,35 +109,32 @@ while($r = mysql_fetch_array($in))
 </tr>
 <tr>
 <td>City</td>
-<td><input type="text" name="City" value="<?php echo $r['City']; ?>" /></td>
+<td><input type="text" name="city" value="<?php echo $r['City']; ?>" /></td>
 </tr>
 <tr>
 <td>State</td>
-<td><input type="text" name="State" value="<?php echo $r['State']; ?>" /></td>
+<td><input type="text" name="state" value="<?php echo $r['State']; ?>" /></td>
 </tr>
 <tr>
 <td>Zip</td>
-<td><input type="text" name="Zip" value="<?php echo $r['ZipCode']; ?>" /></td>
+<td><input type="text" name="zip" value="<?php echo $r['ZipCode']; ?>" /></td>
 </tr>
 <tr>
 <td>Email</td>
-<td><input type="text" name="Email" value="<?php echo $r['decryptedEmail']; ?>" /></td>
+<td><input type="text" name="email" value="<?php echo $r['decryptedEmail']; ?>" /></td>
 </tr>
 <tr>
 <td>About Me</td>
-<td>
-<textarea name="bio" id="bio" value="<?php echo $r['Bio']; ?>" rows="5" style="width:73%" maxlength="300"></textarea>
-</td>
+<td><input type="text" name="bio" rows="5" maxlength="300" value="<?php echo $r['Bio']; ?>" /></td>
+
 </tr>
+<!-- 
 <tr>
 <td>New Password</td>
 <td><input type="text" name="newpass" /></td>
 </tr>
-<tr>
-<td>Login Information:</td>
-<td>Last login: <?php echo $r['last_login']; ?>, total number of logins: <?php echo $r['num_logins']; ?></td>
-</tr>
-<tr>
+ -->
+
 <td colspan="2" align="center">
 <input type="submit" name="update" value="Update Profile" />
 </td>
