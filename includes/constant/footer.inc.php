573965f002ac2fcaf938dfff2dc357fb22a67f22
akern <akern@iastate.edu>
2014-04-27 08:29:14 -0500

    Temporary fix for footer

--- includes/constant/footer.inc.php
+++ includes/constant/footer.inc.php
@@ -1,7 +1,8 @@
 
 <div class="footer">
 
-<p>Pet Project | 333 44th Street, Des Moines, Iowa 50310 | 515-111-2222 | <a href="mailto:contact@petproject.org">contact@petproject.org</a> </p>	
+<p> <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+Pet Project | 333 44th Street, Des Moines, Iowa 50310 | 515-111-2222 | <a href="mailto:contact@petproject.org">contact@petproject.org</a> </p>	
 </div>
 
 
